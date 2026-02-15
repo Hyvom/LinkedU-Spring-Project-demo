ed63e8878c5a104947b236e9da7f3ff47ee28967
Hyvom Rahmouni <hyvomrahmouni00@gmail.com>
2026-02-15 16:56:57 +0100

    Protected endpoints working: added AuthFilter and creation of studentServices + Student Controller

--- src/main/java/com/linkedu/backend/repositories/UserRepository.java
+++ src/main/java/com/linkedu/backend/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.linkedu.backend.repositories;
 
 import com.linkedu.backend.entities.User;
 import com.linkedu.backend.entities.enums.Role;
+import org.aspectj.weaver.loadtime.Agent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -12,5 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     List<User> findByRole(Role role);
     List<User> findByRoleAndEnabled(Role role, boolean enabled);
+    List<User> findByAssignedAgent(User agent);
     Optional<User> findByIdAndEnabled(Long id, boolean enabled);
 }
