d10fb5a432313808d9ea2aa6704dd21e44654143
Hyvom Rahmouni <hyvomrahmouni00@gmail.com>
2026-02-22 18:11:31 +0100

    Documents Workflow Added + New enteties for every Document Type

--- src/main/java/com/linkedu/backend/repositories/DocumentRepository.java
+++ src/main/java/com/linkedu/backend/repositories/DocumentRepository.java
@@ -1,8 +1,9 @@
 package com.linkedu.backend.repositories;
 
-import com.linkedu.backend.entities.Document;
+import com.linkedu.backend.entities.documents.Document;
 import com.linkedu.backend.entities.User;
 import com.linkedu.backend.entities.enums.DocumentStatus;
+import com.linkedu.backend.entities.enums.DocumentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -11,4 +12,5 @@ import java.util.List;
 public interface DocumentRepository extends JpaRepository<Document, Long> {
     List<Document> findByStudent(User student);
     List<Document> findByStudentAndStatus(User student, DocumentStatus status);
+    List<Document> findByStudentAndDocumentType(User student, DocumentType type);
 }
